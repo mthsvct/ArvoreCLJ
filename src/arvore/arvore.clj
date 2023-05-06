42832e1398e96ba4c894bf4ad9e0b950f17c3cca
Matheus Victor <matheusvictor@ufpi.edu.br>
2023-05-06 10:11:46 -0300

    Função de busca

--- src/arvore/arvore.clj
+++ src/arvore/arvore.clj
@@ -34,3 +34,17 @@
             (posOrdem (get raiz :esquerda)) 
             (posOrdem (get raiz :direita))
             (println (pega raiz)))))
+
+(defn busca [raiz valor passos]
+    (cond
+        (nil? raiz) {:no nil :passos passos}
+        (= (pega raiz) valor) {:no raiz :passos passos}
+        (< valor (pega raiz)) (busca (get raiz :esquerda) valor (inc passos))
+        (> valor (pega raiz)) (busca (get raiz :direita) valor (inc passos))))
+
+(defn apresentaBuscar [res]
+    (cond
+        (nil? (get res :no)) (println "O valor não foi encontrado" (str "Quantidade de passos " (get res :passos)))
+        :else (println "O valor foi encontrado!" (get res :no) (str "Quantidade de passos " (get res :passos)))))
+
+(defn gestaoBuscar [raiz] (apresentaBuscar (busca raiz (ler) 0)))
--- src/arvore/core.clj
+++ src/arvore/core.clj
@@ -2,29 +2,28 @@
 	(:gen-class)
 	(:require [arvore.arvore :refer :all]))
 
-
 (defn menu []
+	(println)
 	(println "1 - Inserir")
 	(println "2 - Imprimir em Pre-Ordem")
 	(println "3 - Imprimir em In-Ordem")
 	(println "4 - Imprimir em Pos-Ordem")
+	(println "5 - Buscar um valor" )
 	(println "0 - Sair")
 	(print "Digite uma opcao: ")
 	(flush)
 	(Integer/parseInt (read-line)))
 
 (defn escolhe [raiz op] 
-	(do
-		(cond
-			(= op 1) (escolhe (gestaoLer raiz) (menu))
-			(= op 2) (do (preOrdem raiz) (escolhe raiz (menu)))
-			(= op 3) (do (inOrdem raiz) (escolhe raiz (menu)))
-			(= op 4) (do (posOrdem raiz) (escolhe raiz (menu)))
-			(= op 0) (do (println "Saindo...") raiz)
-			:else (do (println "Opcao invalida!") (escolhe raiz (menu))))))
+	(cond
+		(= op 1) (escolhe (gestaoLer raiz) (menu))
+		(= op 2) (do (preOrdem raiz) (escolhe raiz (menu)))
+		(= op 3) (do (inOrdem raiz) (escolhe raiz (menu)))
+		(= op 4) (do (posOrdem raiz) (escolhe raiz (menu)))
+		(= op 5) (do (gestaoBuscar raiz) (escolhe raiz (menu)))
+		(= op 0) (do (println "Saindo...") raiz)
+		:else (do (println "Opcao invalida!") (escolhe raiz (menu)))))
 
 ; Sem usar loop
-(defn -main [] 
-	(println (escolhe nil (menu)))
-)
+(defn -main [] (println (escolhe nil (menu))))
 
